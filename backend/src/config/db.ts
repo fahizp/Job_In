ee74a659636718a378e3999c9976ed456be76a92
Muhammed Fahiz <fahiz@192.168.0.106>
2024-08-05 18:58:01 +0530

    refactor(config): update database configuration in backend

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const db = async () => {
   const mongoURI = process.env.MONGO_URI as string;
+
   try {
     await mongoose.connect(mongoURI);
-    console.log("db connected");
+
+    console.log('db connected');
   } catch (error) {
-    console.error("connection failed", error);
+    console.error('connection failed', error);
   }
 };
 
