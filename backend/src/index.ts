e869315540a3bc6d1f3b0e086e2cfb9e3f33c565
Muhammed Fahiz <fahiz@192.168.0.106>
2024-08-05 18:47:26 +0530

    test(testing husky): add index.ts file with linting issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,13 @@
-import express  from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 import db from './config/db';
 dotenv.config();
 
 const app = express();
 db();
 
-const PORT = process.env.PORT || 3000;  
+let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
-
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
