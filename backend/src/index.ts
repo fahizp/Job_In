bc02284b277e703cb5405436bf12099faddc6762
Muhammed Fahiz <fahiz@192.168.0.106>
2024-08-05 17:07:21 +0530

    test(husky): Verify pre-commit hook

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,12 @@ dotenv.config();
 const app = express();
 db();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
