80b44be82de5cfecc0b24dd8da2a7471ca9ddfcb
ShuhaibAkthar <akgk75772@gmail.com>
2024-08-05 16:20:22 +0530

    chore(space):reomve unwanded space

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,6 @@ dotenv.config();
 const app = express();
 db();
   
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
