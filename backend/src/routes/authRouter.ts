1db28c987f25e1b48931926cd83e2e9a1a4486e7
ShuhaibAkthar <akgk75772@gmail.com>
2024-08-06 21:51:38 +0530

    feat(auth): Added logout and token refresh routes

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,10 +1,12 @@
 import express from 'express'
-import { userLogin, userSignUp } from '../controllers/authController';
+import { logouting, refreshtoken, userLogin, userSignUp } from '../controllers/authController';
 const router = express.Router();
 
 
 router.post('/signup',userSignUp);
 router.post('/login',userLogin)
+router.post('/refreshtoken',refreshtoken)
+router.post('/logout',logouting)
 
 const authRouter =router
 export default authRouter;
