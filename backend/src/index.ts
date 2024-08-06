209dd8ba7f7999e0704eb78078f6f275aa36fb51
ShuhaibAkthar <akgk75772@gmail.com>
2024-08-06 13:10:54 +0530

    fix(bodyparser): add body-parser middleware to index.ts for JSON parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db from './config/db';
+import authRouter from './routes/authRouter';
+import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
+app.use(express.json()); 
 db();
 
-let PORT = process.env.PORT || 3000;
+app.use('/auth',authRouter)
 
+let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
