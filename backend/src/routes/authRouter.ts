866a3d0d6dbafdcfb492945b92527a7895410847
mhd_safwan <safwanpk2000@gmail.com>
2024-08-06 21:37:19 +0530

    feat: update authentication routes - Updated authRouter.ts to include new routes for user authentication, enhancing the API endpoints for login and registration.

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,10 +1,26 @@
-import express from 'express'
-import { userLogin, userSignUp } from '../controllers/authController';
+import express from 'express';
+import passport from 'passport';
+import { userLogin, userSignUp } from '../controllers/authController'; 
+import { loginSuccess, loginFailed } from '../controllers/googleauth'; 
 const router = express.Router();
+const clientUrl = "http://localhost:3000"; 
 
 
-router.post('/signup',userSignUp);
-router.post('/login',userLogin)
 
-const authRouter =router
-export default authRouter;
+router.post('/signup', userSignUp);
+router.post('/login', userLogin);
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/auth/login/failed',
+  }),
+  (req, res) => {
+    res.redirect(clientUrl);
+  }
+);
+router.get('/login/success', loginSuccess);
+router.get('/login/failed', loginFailed);
+
+
+
+export default router;
