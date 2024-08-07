ccca6af93cd6a1416693418088b90d755dce1bd7
ShuhaibAkthar <akgk75772@gmail.com>
2024-08-07 15:39:02 +0530

    feat(auth):added token verification middleware

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,12 +1,15 @@
 import express from 'express'
-import { logouting, refreshtoken, userLogin, userSignUp } from '../controllers/authController';
+import { logout, protect, refreshingToken, userLogin, userSignUp } from '../controllers/authController';
+import { tokenVerification } from '../middleWare/tokenVerification';
 const router = express.Router();
 
 
 router.post('/signup',userSignUp);
 router.post('/login',userLogin)
-router.post('/refreshtoken',refreshtoken)
-router.post('/logout',logouting)
+router.post('/logout',logout)
+router.get("/protected",tokenVerification,protect);
+router.post("/refreshtoken",refreshingToken)
+
 
 const authRouter =router
 export default authRouter;
