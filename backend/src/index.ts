5fb76f35bcd971f2c07e7802bad9ffb5b58d1e14
mhd_safwan <safwanpk2000@gmail.com>
2024-08-08 10:30:53 +0530

    chore: update index.ts to init server with new auth setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,37 @@
 import dotenv from 'dotenv';
-dotenv.config(); // Load environment variables at the very beginning
+dotenv.config();
+const bodyParser = require('body-parser');
 
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
 import cors from 'cors';
-import db from './config/db'; 
+import db from './config/db';
 import authRouter from './routes/authRouter';
-import './config/passportConfig'; 
+import './controllers/googleauthController';
 
 const app = express();
-db(); 
-
+db();
 
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'secret',
+  secret: process.env.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: true,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/auth', authRouter); 
+app.use('/auth', authRouter);
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
