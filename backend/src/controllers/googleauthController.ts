96a2c43be458681037e2ae6fdc946b395210036c
mhd_safwan <safwanpk2000@gmail.com>
2024-08-12 09:54:41 +0530

    Refactor Google authentication logic in googleauthController

--- backend/src/controllers/googleauthController.ts
+++ backend/src/controllers/googleauthController.ts
@@ -2,10 +2,11 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Request, Response } from "express";
 import User from "../models/authModel";
+import userTokenModel from "../models/userToken";
+import jwt from "jsonwebtoken";
 
 const clientUrl = "http://localhost:3000";
 
-// Define the IUser interface
 export interface IUser {
   id: string;
   name: string;
@@ -14,35 +15,50 @@ export interface IUser {
   googleId: string;
 }
 
-// Passport configuration
 passport.use(
   new GoogleStrategy(
     {
-      // clientID and clientSecret: These are taken from Google Developer Console. They are used to authenticate the application with Google OAuth 2.0.
       clientID: process.env.CLIENT_ID as string,
       clientSecret: process.env.CLIENT_SECRET as string,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "http://localhost:8001/auth/google/callback", 
     },
-    // accessToken: A token that allows access to the user's data.
-    // refreshToken: A token that can be used to obtain a new access token.
-    // profile: The user's profile information taken from Google.
-    // done: A callback function that will be called when authentication is complete.
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({ googleId: profile.id });
-        if (user) {
-          return done(null, user);
-        } else {
-          // If the user does not exist, a new user object is created with the information from the profile
-          const newUser = new User({
+        
+        if (!user) {
+          user = new User({
             name: profile.displayName,
             email: profile.emails[0].value,
             profilePic: profile.photos[0].value,
             googleId: profile.id,
           } as IUser);
-          await newUser.save();
-          return done(null, newUser);
+
+          await user.save();
         }
+
+        const newAccessToken = jwt.sign(
+          { userId: user._id },
+          process.env.ACCESS_TOKEN as string,
+          { expiresIn: process.env.ACCESS_KEY_EXPIRY }
+        );
+
+        const newRefreshToken = jwt.sign(
+          { userId: user._id },
+          process.env.REFRESH_TOKEN as string,
+          { expiresIn: process.env.REFRESH_KEY_EXPIRY }
+        );
+
+        await userTokenModel.findOneAndDelete({ userId: user._id });
+        await new userTokenModel({
+          userId: user._id,
+          token: newRefreshToken,
+        }).save();
+
+        (user as any).accessToken = newAccessToken;
+        (user as any).refreshToken = newRefreshToken;
+
+        return done(null, user);
       } catch (error) {
         console.error("Error during Google authentication:", error);
         return done(error);
@@ -55,31 +71,29 @@ passport.serializeUser((user: IUser, done) => {
   done(null, user.id);
 });
 
-// user.id is stored in the session
 passport.deserializeUser(async (id: string, done) => {
   try {
-    const user = (await User.findById(id)) as IUser;
+    const user = await User.findById(id) as IUser;
     done(null, user);
   } catch (error) {
     done(error);
   }
 });
 
-// This function is called when a user successfully logs in
 export const loginSuccess = (req: Request, res: Response) => {
   if (req.user) {
-    return res.status(200).json({ user: req.user });
+    const { accessToken, refreshToken } = req.user as any;
+    return res.status(200).json({ user: req.user, accessToken }); // Send access token
   }
   return res.status(404).json({ message: "Not authorized" });
 };
 
-// This function is called after the user is authenticated with Google and redirects to clientUrl (3000)
 export const googleCallback = (req: Request, res: Response) => {
   res.redirect(clientUrl);
 };
 
-// This function is called when login fails
 export const loginFailed = (req: Request, res: Response) => {
   return res.status(401).json({ message: "Log in failure" });
 };
+
 
