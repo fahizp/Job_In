687581c170919c4d44818850b4689f0eef787726
mhd_safwan <safwanpk2000@gmail.com>
2024-08-13 12:30:41 +0530

    feat: (remove google route) remove some  routes in authRouter

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,36 +1,18 @@
 import express from "express";
-import passport from "passport";
-import {
-  loginSuccess,
-  loginFailed,
-  googleCallback,
-} from "../controllers/googleauthController";
 import {
   userSignUp,
   userLogin,
   logout,
   protect,
   refreshingToken,
+  nameCheck
 } from "../controllers/authController";
 import { tokenVerification } from "../middleWare/tokenVerification";
-const { body, validationResult } = require("express-validator");
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-router.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/auth/login/failed" }),
-  googleCallback
-);
 
-router.get("/login/success", loginSuccess);
-router.get("/login/failed", loginFailed);
-// router.post('/forgot-password',forgotPassword);
-// router.post('/reset-password/:id', reset_password);
 
 router.post(
   "/signup",
@@ -43,14 +25,16 @@ router.post(
     .withMessage("Please Enter a valid email address.")
     .normalizeEmail(),
 
-  body("password", "Password should contain atleast 5 alphanumeric characters.")
+  body("password", "Password should contain at least 5 alphanumeric characters.")
     .isLength({ min: 5 })
     .isAlphanumeric(),
   userSignUp
 );
+
 router.post("/login", userLogin);
 router.post("/logout", logout);
 router.get("/protected", tokenVerification, protect);
 router.post("/refreshtoken", refreshingToken);
+router.post("/nameCheck", nameCheck);
 
 export default router;
