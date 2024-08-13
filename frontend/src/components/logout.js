f909f961b8f801463121b3089d89ce6b738cb279
mhd_safwan <safwanpk2000@gmail.com>
2024-08-13 12:34:45 +0530

    feat: (add tost-logout) implement logout functionality in logout.js

--- frontend/src/components/logout.js
+++ frontend/src/components/logout.js
@@ -1,12 +1,18 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Logout = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        localStorage.removeItem("accessToken");
-        navigate("/");
+        const handleLogout = () => {
+            localStorage.removeItem("accessToken");
+            toast.success("Logged out successfully.");
+            navigate("/");
+        };
+
+        handleLogout();
     }, [navigate]);
 
     return null;
