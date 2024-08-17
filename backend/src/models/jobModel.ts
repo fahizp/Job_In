0b975fbeedf35d924ff86c51a8c24e787acfcfe4
ShuhaibAkthar <akgk75772@gmail.com>
2024-08-17 16:39:49 +0530

    feat(job):build job apply schema & model

---
+++ backend/src/models/jobModel.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const jobApplySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      sparce: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    typesOfJobs: {
+      type: String,
+    },
+
+    description: {
+        type: String,
+        maxLength: 220,
+      },
+    phoneNumber: {
+      type: Number,
+    },
+    cv: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const jobApplyModel= mongoose.model('Job applies', jobApplySchema);
+export default jobApplyModel;
