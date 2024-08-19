650dddbc03f637ae09f50a908bb89985571b48da
mhd_safwan <safwanpk2000@gmail.com>
2024-08-19 19:30:09 +0530

    feat(google auth add): enhance login page functionalit

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { GoogleLogin } from '@react-oauth/google';
 import * as Yup from "yup";
 import { ToastContainer, toast } from "react-toastify"; 
 import "react-toastify/dist/ReactToastify.css"; 
@@ -65,6 +66,29 @@ export default function Login() {
         }
     };
 
+
+    const handleGoogleSuccess = async (credentialResponse) => {
+        try {
+          const response = await axios.post('http://localhost:8001/auth/google', {
+            token: credentialResponse.credential,
+          });
+  
+          localStorage.setItem("accessToken", response.data.accessToken);
+          localStorage.setItem("refreshToken", response.data.refreshToken);
+          toast.success("Login successful!");
+  
+          navigate('/index');
+        } catch (error) {
+          console.error("Google login failed:", error);
+          toast.error("Google login failed. Please try again.");
+        }
+      };
+
+      const handleGoogleFailure = () => {
+        toast.error("Google login failed. Please try again.");
+      };
+
+
     return (
         <>
             <ToastContainer />
@@ -74,7 +98,7 @@ export default function Login() {
                     <div className="row">
                         <div className="col-lg-4 col-md-5 col-12">
                             <div className="p-4 bg-white rounded shadow-md mx-auto w-100" style={{ maxWidth: '400px' }}>
-                                <form onSubmit={handleSubmit}>
+                                <form onSubmit={handleSubmit}>                                  
                                     <Link to="/"><img src={logo} className="mb-4 d-block mx-auto" alt=""/></Link>
                                     <h6 className="mb-3 text-uppercase fw-semibold">Please sign in</h6>
 
@@ -124,6 +148,13 @@ export default function Login() {
                                             <span className="text-muted me-2 small">Don't have an account?</span>
                                             <Link to="/signup" className="text-dark fw-semibold small">Sign Up</Link>
                                         </span>
+                                        <div className="google">
+                                            <GoogleLogin  className="gbutton"
+                                            onSuccess={handleGoogleSuccess}
+                                            onError={handleGoogleFailure}
+                                        />
+                                        </div>
+                                        
                                     </div>
                                 </form>
                             </div>
