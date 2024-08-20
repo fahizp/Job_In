e196f88fb467ca7bda9d46a4644e4fd058f0728a
ShuhaibAkthar <akgk75772@gmail.com>
2024-08-20 06:19:52 +0530

    feat(job post):added  job  submission

--- backend/src/controllers/jobController.ts
+++ backend/src/controllers/jobController.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { jobInterface } from '../utils/typos';
-import jobApplyModel from '../models/jobModel';
+import { jobApplyModel, jobPostModel } from '../models/jobModel';
 import { validationResult } from 'express-validator';
 
 //job apply
@@ -78,19 +78,22 @@ export const jobApply = async (req: express.Request, res: express.Response) => {
 // job list
 export const jobList = async (req: express.Request, res: express.Response) => {
   try {
-    //getting job list from job collection
-    // const jobList = await jobApplyModel.aggregate([
-    //   {
-    //     $project: {
-    //       jobTitle: 1,
-    //       typesOfJobs: 1,
-    //       Country: 1,
-    //       logo: 1,
-    //     },
-    //   },
-    // ]);
+    // getting job list from job collection
+    const jobList = await jobPostModel.aggregate([
+      {
+        $project: {
+          title: 1,
+          jobCategory: 1,
+          Country: 1,
+          logo: 1,
+          minSalary: 1,
+          maxSalary: 1,
+          postedDate: 1,
+        },
+      },
+    ]);
 
-    return res.status(200).json({ jobList: 'jobList' });
+    return res.status(200).json({ jobList: jobList });
   } catch (error) {
     console.error('Error on fetching job list:', error);
     res.status(500).send('Internal server error');
@@ -106,9 +109,9 @@ export const jobDetails = async (
   const userId = req.params.id;
 
   try {
-    //updating user contact details
-    // const  jobDetails = await authModel.findById(userId);
-    return res.status(200).json({ jobDetails: 'jobDetails' });
+    //fetching job details
+    const jobDetails = await jobPostModel.findById(userId);
+    return res.status(200).json({ jobDetails: jobDetails });
   } catch (error) {
     console.error('Error on fetching job details:', error);
     res.status(500).send('Internal server error');
@@ -127,24 +130,123 @@ export const jobSearch = async (
     const regex = new RegExp(searchQuery, 'i');
 
     //aggregation for search job
-    // const results = await authModel.aggregate([
-    //   {
-    //     $match:{
-    //       $or:[
-    //         {name:{$regex:regex}},
-    //         {name:{$regex:regex}},
-    //         {name:{$regex:regex}},
-    //       ]
-    //     }
-    //   },
-    //   {$sort:{name:1}},
-    //   {$project:{
-    //     name:1
-    //   }}
-    // ])
-    return res.status(200).json({ results: 'results' });
+    const results = await jobPostModel.aggregate([
+      {
+        $match:{
+          $or:[
+            {title:{$regex:regex}},
+            {country:{$regex:regex}},
+            {jobCategory:{$regex:regex}},
+          ]
+        }
+      },
+      // {$sort:{name:1}},
+      {$project:{
+        title:1
+      }}
+    ])
+    return res.status(200).json({ results: results });
   } catch (error) {
     console.error('Error on searching jobs:', error);
+    res.status(500).send('Internal server error');
+  }
+};
+
+//job post
+export const jobPost = async (req: express.Request, res: express.Response) => {
+  // Retrieve and cast environment variables for S3 bucket configuration
+  const bucketName = process.env.BUCKET_NAME as string;
+  const bucketRegion = process.env.BUCKET_REGION as string;
+  const s3AccessKey = process.env.S3_ACCESS_KEY as string;
+  const s3SecretAccessKey = process.env.S3_SECRET_ACCESS_KEY as string;
+
+  //configuring an AWS S3 client
+  const s3 = new S3Client({
+    credentials: {
+      accessKeyId: s3AccessKey,
+      secretAccessKey: s3SecretAccessKey,
+    },
+    region: bucketRegion,
+  });
+
+  try {
+    //taking values from req.body
+    const {
+      title,
+      companyName,
+      jobType,
+      country,
+      state,
+      jobCategory,
+      minSalary,
+      maxSalary,
+      experience,
+      qualification,
+      responsibilities,
+      description,
+      Requireds,
+      Industry,
+      address,
+    }: jobInterface = req.body;
+
+    //taking companylogo from req.file
+    const logo = req.file;
+
+    //checking the required field
+    if (
+      !title ||
+      !companyName ||
+      !jobType ||
+      !jobCategory ||
+      !country ||
+      !state ||
+      !experience ||
+      !qualification ||
+      !description ||
+      !maxSalary ||
+      !minSalary
+    ) {
+      return res.status(400).send('Missing required fields');
+    }
+
+    // Upload Profile Photo
+    const logoParams = {
+      Bucket: bucketName,
+      Key: logo?.originalname,
+      Body: logo?.buffer,
+      ContentType: logo?.mimetype,
+    };
+    const logoCommand = new PutObjectCommand(logoParams);
+    await s3.send(logoCommand);
+
+    // Create a new user
+    const newjob = new jobPostModel({
+      title,
+      companyName,
+      jobType,
+      country,
+      state,
+      jobCategory,
+      maxSalary,
+      minSalary,
+      experience,
+      qualification,
+      responsibilities,
+      description,
+      Requireds,
+      Industry,
+      address,
+      logo: `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/${logo?.originalname}`,
+    });
+
+    //saving new candidate
+    await newjob.save();
+    res.status(200).json({
+      message: 'job created successfully',
+      candidate: newjob,
+    });
+  } catch (error) {
+    console.error('Error handling form submission:', error);
     res.status(500).send('Internal server error');
   }
 };
