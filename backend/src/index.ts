ed37d7ae2257f1ea452331d43a4b5aaa4744f2db
mhd_safwan <safwanpk2000@gmail.com>
2024-08-28 16:39:41 +0530

    feat(core): update index file for better app initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,17 +11,17 @@ import jobRouter from './routes/jobRouter';
 import homeRouter from './routes/homeRouter'
 const app = express();
 db();
-
+app.use(express.json());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: 'http://localhost:3000',
     credentials: true,
   }),
 );
-app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/auth', authRouter);
 app.use('/candidate', candidateRouter);
