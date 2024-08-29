ab0402f343fe9d10d6cef793bcaddb5da3c2aed5
mhd_safwan <safwanpk2000@gmail.com>
2024-08-29 15:20:14 +0530

    feat(pages): improve contact us page for better communication

--- frontend/src/pages/contactus.js
+++ frontend/src/pages/contactus.js
@@ -210,48 +210,10 @@ export default function ContactUs() {
           </div>
         </div>
 
-        <div className="container mt-100 mt-60">
-          <div className="row g-4">
-            {contactData.map((item, index) => {
-              let Icon = item.icon;
-              return (
-                <div className="col-md-4" key={index}>
-                  <div className="position-relative features text-center mx-lg-4 px-md-1">
-                    <div className="feature-icon bg-soft-primary rounded shadow mx-auto position-relative overflow-hidden d-flex justify-content-center align-items-center">
-                      <Icon className="fea icon-ex-md" />
-                    </div>
+        
+        
 
-                    <div className="mt-4">
-                      <h5 className="mb-3">{item.title}</h5>
-                      <p className="text-muted">{item.desc}</p>
-                      <Link to={item.link} className="text-primary">
-                        {item.link}
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="container-fluid mt-100 mt-60">
-          <div className="row">
-            <div className="col-12 p-0">
-              <div className="card map border-0">
-                <div className="card-body p-0">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
-                    title="job_in"
-                    style={{ border: 0 }}
-                    className="rounded"
-                    allowFullScreen=""
-                  ></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
       </section>
 
       <Footer />
